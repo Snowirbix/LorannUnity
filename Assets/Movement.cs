41dcaf7f6a0b27149b9c16e69e2d405c5ff2be56
Alban Gaudin <snowirbix@gmail.com>
2019-03-23 21:36:02 +0100

    Init

---
+++ Assets/Movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+
+public class Movement : MonoBehaviour
+{
+    protected Tilemap tilemap;
+    protected List<Vector2Int> anim = new List<Vector2Int>();
+    protected Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        tilemap = FindObjectOfType<Tilemap>();
+        // blend tree values
+        anim.Add(new Vector2Int( 0, -1));
+        anim.Add(new Vector2Int(-1, -1));
+        anim.Add(new Vector2Int(-1,  0));
+        anim.Add(new Vector2Int(-1,  1));
+        anim.Add(new Vector2Int( 0,  1));
+        anim.Add(new Vector2Int( 1,  1));
+        anim.Add(new Vector2Int( 1,  0));
+        anim.Add(new Vector2Int( 1, -1));
+    }
+
+    private void FixedUpdate()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+
+        var pos = transform.position;
+        pos.x += x;
+        pos.y += y;
+
+        if (tilemap.GetColliderType(new Vector3Int((int)(pos.x-0.5f), (int)(pos.y-0.5f), 0)) == Tile.ColliderType.None)
+        {
+            transform.position = pos;
+        }
+
+        if (x == 0 && y == 0)
+        {
+            // get current anim values
+            x = animator.GetFloat("x");
+            y = animator.GetFloat("y");
+            // find current pose
+            var i = anim.FindIndex(v => v.Equals(new Vector2Int((int)x, (int)y)));
+            // following pose
+            i = (i + 1) % 8;
+            // set following anim values
+            x = anim[i].x;
+            y = anim[i].y;
+        }
+        animator.SetFloat("x", x);
+        animator.SetFloat("y", y);
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0.5f, 0.5f, -0.02f);
+    public float tick = 0.13f;
+    public Tilemap tilemap;
+
+    private float time = 0;
+    private Animator anim;
+    private Vector3Int position;
+    private float x = 1;
+    private float y = 0;
+    private float animTime = 0;
+
+    void Start ()
+    {
+        position = Vector3Int.FloorToInt(transform.position);
+        anim = GetComponent<Animator>();
+    }
+	
+	void Update ()
+    {
+        time += Time.deltaTime;
+        animTime += Time.deltaTime;
+
+        if (time > tick)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            if (horizontal != 0 || vertical != 0)
+            {
+                Vector3Int directionX = new Vector3Int((int)horizontal, 0, 0);
+                Vector3Int directionY = new Vector3Int(0, (int)vertical, 0);
+
+                if (tilemap.GetColliderType(position + directionX + directionY) == Tile.ColliderType.None)
+                {
+                    position = position + directionX + directionY;
+                    transform.position = position + offset;
+                    time = 0;
+                }
+                else if (tilemap.GetColliderType(position + directionX) == Tile.ColliderType.None)
+                {
+                    position = position + directionX;
+                    transform.position = position + offset;
+                    time = 0;
+                }
+                else if (tilemap.GetColliderType(position + directionY) == Tile.ColliderType.None)
+                {
+                    position = position + directionY;
+                    transform.position = position + offset;
+                    time = 0;
+                }
+
+                x = Vector3.Normalize(directionX + directionY).x;
+                y = Vector3.Normalize(directionX + directionY).y;
+            }
+            else
+            {
+                if (animTime > tick)
+                {
+                    animTime = 0;
+                    float angle = Mathf.Atan2(y, x) - (Mathf.PI / 4);
+                    x = Mathf.Cos(angle);
+                    y = Mathf.Sin(angle);
+                }
+            }
+
+            anim.SetFloat("x", x);
+            anim.SetFloat("y", y);
+        }
+    }
+
+    void OnTriggerEnter2D ()
+    {
+        Debug.Log("trigger");
+    }
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        Debug.Log("collide");
+    }
+}
