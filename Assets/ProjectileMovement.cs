fe1a57437ecd3c6b629d211ff64c0dfad45b35a0
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2019-03-31 23:12:34 +0200

    Fireball added (the player can shoot) + fix anim fireball issue
    So, now the player can shoot in any direction by pressing "z,q,s,d". The player has to wait until the "projectile" get destroyed (touching  a wall) before re-firing .The fireball has his animation fixed.

---
+++ Assets/ProjectileMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    protected Tilemap tilemap;
+    protected Animator animator;
+    protected Vector2 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = FindObjectOfType<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var pos = transform.position;
+        pos.x += dir.x;
+        pos.y += dir.y;
+
+        if (tilemap.GetColliderType(new Vector3Int((int)(pos.x-0.5f), (int)(pos.y-0.5f), 0)) == Tile.ColliderType.None)
+        {
+            transform.position = pos;
+        }else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetDir(Vector2 direction){
+        this.dir = direction;
+    }
+}
---
+++ Assets/Tir.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tir : MonoBehaviour
+{
+    public GameObject ball;
+    protected Animator animBall;
+    protected ProjectileMovement projScript;
+
+    protected GameObject instantBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animBall = ball.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var x = Input.GetAxisRaw("Fire1");
+        var y = Input.GetAxis("Fire2");
+
+        if( (x != 0 || y != 0) && !GameObject.Find(ball.name + "(Clone)")){
+            instantBall = Instantiate(ball, new Vector3(transform.position.x + x , transform.position.y + y , transform.position.z), Quaternion.identity );
+
+            instantBall.AddComponent(typeof(ProjectileMovement));
+            projScript = instantBall.GetComponent<ProjectileMovement>();
+            projScript.SetDir(new Vector2(x,y));
+        }
+    }
+}
